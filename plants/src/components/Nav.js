a764abd3117f95bb0af4b61a12e454fe7cab6a0e
tdubs42 <twessar42@gmail.com>
2021-04-21 14:00:12 -0700

    Added CSS reset cleaned up code in index.js (swapped ReactDOM.render with render, put Router around App component as taught in class) changed 'div' in index.html to 'main' to aid SEO and screen reader accessibility added nav styling and responsiveness based on PVD added Switch and Routes to App component: ensured they are firing correctly on nav click changed Links in nav to NavLinks from react-router-dom

--- plants/src/components/Nav.js
+++ plants/src/components/Nav.js
@@ -1,28 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React       from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Nav.css";
 
 const Nav = () => {
-  return (
-    <Router>
-      <nav >
-        <div >
-          <h1 className='nav-header'>Water Your Plants!</h1>
-        </div>
-        <div className="navBar">
-          <Link to="/">
-            My Plants
-          </Link>
-          <Link to="/">
-            Add Plant
-          </Link>
-          <Link to="/">
-            Logout
-          </Link>
-        </div>
-      </nav>
-      <br></br>
-    </Router>
-  );
+    return (
+        <section className="nav-container">
+            <header className="nav-header-container">
+                <h1 className="nav-header">Water Your Plants!</h1>
+            </header>
+            <nav>
+                <NavLink to="/my-plants" className="nav-links">
+                    My Plants
+                </NavLink>
+                <NavLink to="/add-plant" className="nav-links">
+                    Add Plant
+                </NavLink>
+                <NavLink to="/" className="nav-links">
+                    Logout
+                </NavLink>
+            </nav>
+        </section>
+    );
 };
 
 export default Nav;
--- plants/src/index.js
+++ plants/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
+import './styles/reset.css';
 
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+render(
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('root')
 );
-
