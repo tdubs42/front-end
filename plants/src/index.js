9dc226b050a326926a72ea07b6fde06b604f9b80
Andrew Castillo <72419774+Alkaede@users.noreply.github.com>
2021-04-21 14:14:15 -0700

    I'm dumb, <Routing> Fixed on Index.Js

--- plants/src/index.js
+++ plants/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './styles/reset.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-render(<App />, document.getElementById('root'));
+render(
+  <Router> 
+    <App />
+  <Router/>, 
+  document.getElementById('root')
+);
